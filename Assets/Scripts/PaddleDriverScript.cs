070f579e0db7552e7a3b7bd1fd01b2b3f404daf3
Kenneth M Casey <KennethMCasey@Gmail.com>
2020-03-11 13:17:52 -0400

    inital

---
+++ Assets/Scripts/PaddleDriverScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleDriverScript : MonoBehaviour {
+
+    public float hitPower = 0f;
+    public float paddleDamper = 0f;
+    public float upPosition = 45f;
+    public float downPosition = 0f;
+
+    public HingeJoint hinge;
+    public string inputName;
+
+
+	// Use this for initialization
+	void Start () {
+
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        JointSpring spring = new JointSpring
+        {
+            spring = hitPower,
+            damper = paddleDamper
+        };
+        spring.targetPosition =
+                  System.Math.Abs( Input.GetAxis(inputName)-1)
+                  <0.001f ? upPosition : downPosition;
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+		
+	}
+}
